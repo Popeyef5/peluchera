0b13967a7f2affa05abed93ef7a08c346b8fa6e6
Gaston Maffei <gastonmaffei@gmail.com>
2025-08-08 21:03:24 -0300

    umami

--- central/next/app/layout.tsx
+++ central/next/app/layout.tsx
@@ -6,6 +6,7 @@ import { headers } from "next/headers"
 import ContextProvider from "@/context";
 import ChakraProvider from "@/components/chakra/provider"
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,19 +28,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProd = process.env.NODE_ENV === "production";
 
   const headersObj = await headers();
   const cookies = headersObj.get('cookie')
 
   return (
     <html lang="en" suppressHydrationWarning>
+      {isProd && <Script defer src={process.env.NEXT_PUBLIC_UMAMI_SRC} data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID} />}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ContextProvider cookies={cookies}>
           <ChakraProvider>
             {children}
-            <Toaster/>
+            <Toaster />
           </ChakraProvider>
         </ContextProvider>
       </body>
