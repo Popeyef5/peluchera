30219b08e193a23d1e8757bff8ca0a0998ee39ce
Gaston Maffei <gastonmaffei@gmail.com>
2025-08-09 17:46:49 -0300

    changes

--- central/next/instrumentation-client.ts
+++ central/next/instrumentation-client.ts
@@ -1,9 +1,6 @@
 import posthog from "posthog-js";
 
-console.log("checking environment")
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
-  console.log("Initiating posthog")
-  console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === "production") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: "2025-05-24",
